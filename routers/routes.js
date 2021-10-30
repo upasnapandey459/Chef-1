75198bad47d2deae9a4b31c1afdc0e181818fda0
suryanjain14 <suryanjain14@gmail.com>
2021-10-30 18:26:34 +0530

    added get user by id

--- routers/routes.js
+++ routers/routes.js
@@ -7,9 +7,14 @@ router.get("/check",(req,res)=>{
     return res.send("Check OK")
 });
 
+router.get("/dish-get",dishController.dishGet);
+
+router.get("/dish-get/id:id" ,dishController.dishGetByID);
+
 router.post("/signUp",userController.signUp);
 
 router.post('/dish-add',dishController.dishadd);
+
 
 
 module.exports = router
