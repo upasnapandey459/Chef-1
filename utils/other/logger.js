a0c7ccb51e7da5e2ff804b769dcb5c3fe3242ba6
Sohail22360 <sohail.malik@quantiphi.com>
2021-11-22 19:05:25 +0530

    logger modified

--- utils/other/logger.js
+++ utils/other/logger.js
@@ -1,7 +1,9 @@
 const config = require('../../config/config');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, printf } = format;
- 
+// const { add } = createLogger;
+// const winstonRotator = require('winston-daily-rotate-file');
+
 const loggerFormat = printf(info => {
     return `${info.timestamp} | ${info.level}: ${info.message}`;
 });
@@ -16,6 +18,26 @@ const logger = createLogger({
     transports: [
         new transports.Console()
     ]
-    });
+});
+
+// const successLogger = logger;
+// successLogger.add(new winstonRotator({
+//   'name': 'success-file',
+//   'level': 'info',
+//   'filename': '../../logs/success.log',
+//   'json': false,
+//   'datePattern': 'yyyy-MM-dd-',
+//   'prepend': true
+// }));
+
+// const errorLogger = logger;
+// errorLogger.add(new winstonRotator({
+//   'name': 'error-file',
+//   'level': 'error',
+//   'filename': "../../logs/error.log",
+//   'json': false,
+//   'datePattern': 'yyyy-MM-dd-',
+//   'prepend': true
+// }));
 
 module.exports = logger;
